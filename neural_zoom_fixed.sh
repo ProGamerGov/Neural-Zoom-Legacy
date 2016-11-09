f792536e25b01d5ac39ebff5a465e1f79610564d
ProGamerGov <ProGamerGov@users.noreply.github.com>
2016-11-09 14:18:39 -0700

    Cleaned up working code

--- neural_zoom_fixed.sh
+++ neural_zoom_fixed.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 
-#https://gist.github.com/ProGamerGov/7b818549254065d1ae2014774a821feb
-
- Check for output directory, and create it if missing
+ #Check for output directory, and create it if missing
 if [ ! -d "$output" ]; then
   mkdir output
 fi
+
 
 main(){
     # 1. input image
@@ -19,6 +18,7 @@ main(){
     style_file=`basename $style`
     style_name="${style_file%.*}"
     
+    #Defines the output directory
     output="./output"
 
     # 4. Zoom value
@@ -30,13 +30,6 @@ main(){
     #6. Number of frames
     num_frames=$5
 
-    #width=`convert $input_file -format "%w" info:`
-    #height=`convert $input_file -format "%h" info:`
-
-#Calculate the new cropped dimensions of the new image
-#widthcrop=`echo $width $zoom | awk '{print $1-$2}'`
-#heightcrop=`echo $height $zoom | awk '{print $1-$2}'`
-
 ###############################################
 
 #Run image 0 through neural_style.lua before cropping/rotation occurs
@@ -44,11 +37,6 @@ main(){
 v_frames=0
 out_file1="${v_frames}_${clean_name}.png"
 neural_style $input $style $out_file1
-
-
-#neural_style $input $style $out_file
-
-#input=$outfile1 
 
 ###############################################
 
@@ -62,41 +50,33 @@ heightcrop=`echo $height $zoom | awk '{print $1-$2}'`
 
 ###############################################
 
-#Take output image 0 and rotate/crop it before running the new output through neural_style.lua again. 
+#Takes output image 0 and rotates/crops it before running the new output through neural_style.lua again. 
 #This process is repeated until it has been done a use specified number of times.
 
 for r in `seq 1 $num_frames`;
 do 
 
-#Get the width and height  
-#width=`convert "0_${clean_name}.png" -format "%w" info:`
-#height=`convert "0_${clean_name}.png" -format "%h" info:`
-#Calculate the new cropped dimensions of the new image
-#widthcrop=`echo $width $zoom | awk '{print $1-$2}'`
-#heightcrop=`echo $height $zoom | awk '{print $1-$2}'`
-
-
-
-input=$outfile 
+input=$outfile
 
 convert -rotate $rotation_value -gravity center -crop "$widthcrop"x"$heightcrop"+0+0 $out_file "${v_frames}_${clean_name}.png"
-
 input="${v_frames}_${clean_name}.png"
-
 v_frames=`echo $v_frames 1 | awk '{print $1+$2}'`
-
 out_file="${v_frames}_${clean_name}.png"
 
 neural_style $input $style $out_file
 
+#Copy the uncropped output to the output directory in order to preserve it's output size
 cp $out_file $output/$out_file
- 
 
 done
 
 }
 
 retry=0
+
+###############################################
+
+#Specific the Neural-Style command used for each frame here:
 
 neural_style(){
     echo "Neural Style Transfering "$1
